bf2343ba5ee86e4aaa43a69e4b4bf3ba961b7272
aryunewaskar77-art <aryu.newaskar77@gmail.com>
2026-01-30 11:32:27 +0530

    weather app

--- lec7/weatherApp/script.js
+++
@@ -1,8 +0,0 @@
-
-let btn = document.getElementById("btn");
-btn.addEventListener("click",()=>{
-    const inputValue = document.getElementById("input");
-    var input = inputValue.value;
-    console.log(input);
-    inputValue.value = "";
-})
